586e6b7642d09934d8b7d6eb6855522d3247b331
Mr Black <Beelin@mantis-macbook-pro.local>
2017-06-03 23:22:48 +0800

    Initial Commit

---
+++ XXAFNetWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXAFNetWorking
+//
+//  Created by Beelin on 2017/6/3.
+//  Copyright © 2017年 Beelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXAFNetWorking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXAFNetWorking
+//
+//  Created by Beelin on 2017/6/3.
+//  Copyright © 2017年 Beelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
